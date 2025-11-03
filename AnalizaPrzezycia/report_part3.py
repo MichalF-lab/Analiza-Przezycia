f7053476df041032085824af122a619adf684fe2
Michał Frąckowiak <micha-lfrackowiak@wp.pl>
2025-11-03 23:54:38 +0100

    bugfix

--- AnalizaPrzezycia/report_part3.py
+++ AnalizaPrzezycia/report_part3.py
@@ -1,5 +1,8 @@
+from math import gamma
 import numpy as np
-from scipy.stats import beta
+from numpy.random import seed
+from scipy.stats import beta, gamma
+from report_part2 import first_type_error
 # from report_part1 import wzor_do_base64
 
 # --- Dane dla leku A ---
@@ -57,23 +60,20 @@ print(f'L_A: {L_A}')
 print(f'L_B: {L_B}')
 
 # 1B
-def binom_confint(x, alpha=0.05):
-    n = len(A)
-    lower = np.where(x>0, beta.ppf(alpha / 2, x, n - x + 1), 0.0)
-    upper = np.where(x<n, beta.ppf(1 - alpha / 2, x + 1, n - x), 1.0)
-    return (lower, upper)
+def confidence_interval_type1(data, alpha):
+    n = len(data)
+    R = np.sum(data < np.max(data))
+    temp_L = beta.ppf(alpha / 2, R, n - R + 1)
+    temp_U = beta.ppf(1 - alpha / 2, R + 1, n - R)
+    theta_L = -np.log(1 - temp_L)
+    theta_U = -np.log(1 - temp_U)
 
-def confidence_interval(data, alpha=0.05):
-    ext = binom_confint(data, alpha)
-    Tl, Tu = ext
-    Tl = - np.log(1 - Tl)
-    Tu = - np.log(1 - Tu)
-    return (Tl, Tu)
+    return theta_L, theta_U
 
-print(confidence_interval(A, 0.05)) #0.76 #3.15
-print(confidence_interval(B, 0.05))
-print(confidence_interval(A, 0.01))
-print(confidence_interval(B, 0.01))
+print(confidence_interval_type1(A, 0.05)) #0.76 #3.15
+print(confidence_interval_type1(B, 0.05))
+print(confidence_interval_type1(A, 0.01))
+print(confidence_interval_type1(B, 0.01))
 
 # binom.confint(x,n,conflevel = 1-alpha, method = 'exact')
 # do przedzialu ufności
@@ -93,9 +93,31 @@ print(f'L_Acenzored: {L_A}') # 0.73
 print(f'L_Bcenzored: {L_B}') # 0.96
 
 # 2B
-def confidence_interval_censored(alpha, data, n_censored):
-    alpha = alpha / 2
-    p = 1 - np.exp()
-    Tl = 1
-    Tu = 1
-    return (Tl, Tu)
+def confidence_interval_type2(data, alpha):
+    n = len(data)
+    m = np.sum(data < np.max(data))
+    data = np.sort(data)
+    data_uncenzored = data[:m]
+
+    sum_di = 0
+    for i in range(len(data_uncenzored)):
+        sum_di += (len(data) - i + 1) * (data_uncenzored[i] - data_uncenzored[i - 1]) if i > 0 else (len(data) - i + 1) * data_uncenzored[i]
+
+    sum_Di = np.sum(data_uncenzored) + (n - m) * data[m - 1]
+    temp_L = gamma.ppf(alpha / 2, a=m, scale=1/m)
+    temp_U = gamma.ppf(1 - (alpha / 2), a=m, scale=1/m) 
+    theta_L = (m / sum_Di) * temp_L
+    theta_U = (m / sum_Di) * temp_U
+
+    return theta_L, theta_U
+
+print(confidence_interval_type2(A, 0.05))
+print(confidence_interval_type2(B, 0.05))
+print(confidence_interval_type2(A, 0.01))
+print(confidence_interval_type2(B, 0.01))
+
+np.random.seed(42)
+temp1 = first_type_error(0.5, n=10)
+temp2 = first_type_error(0.5, n=30)
+temp3 = first_type_error(1.2, n=10)
+temp4 = first_type_error(1.2, n=30)
