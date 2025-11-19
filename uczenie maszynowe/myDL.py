8a4d1e105f944e3c115497d797d3675c593dba67
Michał Frąckowiak <micha-lfrackowiak@wp.pl>
2025-11-19 14:28:30 +0100

    start my own Dl

---
+++ uczenie maszynowe/myDL.py
@@ -0,0 +1,12 @@
+
+class neuron():
+    def __init__(self, rozmiar_poprzednije):
+        self.rozmiar = rozmiar
+        self.bias = [np.random.randn()]
+        self.wagi = [np.random.randn(x) for x in range(rozmiar_poprzedniej)]
+
+class siec():
+    def __init__(self, siec_wymiary):
+        self.siec = siec
+
+        
